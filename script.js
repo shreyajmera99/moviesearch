b32d0d0b2012664518d7f7aa8a179e8a903d9d64
shreyajmera99 <ajmerashrey83@gmail.com>
2023-06-27 20:34:37 +0530

    Api error handling

--- script.js
+++ script.js
@@ -5,46 +5,50 @@ const resultDiv = document.getElementById('result');
 
 // Update the innerHTML of the <div> element with the image
 let key = "2921c1e";
-let fetchmovie=()=>{
-    let movienames = moviename.value
-    // let url = `https://www.omdbapi.com/t=${movienames}&apikey=2921c1e`
-    if(movienames.length <=0){
-        resultDiv.innerHTML = `<h3 class "msg">Please Enter a Movie name</h3>`
+  
+  let fetchmovie = () => {
+    let movieName = moviename.value;
+    let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
 
+    if (movieName.length <= 0) {
+      resultDiv.innerHTML = `<h3 class="msg">Please Enter a Movie Name</h3>`;
+    } else {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          if (data.Response === 'True') {
+            resultDiv.innerHTML = `
+              <div class="info">
+                <img src="${data.Poster}" class="poster">
+                <h2>${data.Title}</h2>
+                <div class="rating">
+                  <img src="star1.png">
+                  <h4>${data.imdbRating}</h4>
+                </div>
+                <div class="details">
+                  <span>${data.Rated}</span>
+                  <span>${data.Year}</span>
+                  <span>${data.Runtime}</span>
+                </div>
+                <div class="genre">
+                  <div>${data.Genre.split(",").join("</div><div>")}</div>
+                </div>
+                <div class="actors">
+                  <div>${data.Actors.split(",").join("</div><div>")}</div>
+                </div>
+              </div>
+            `;
+          } else {
+            resultDiv.innerHTML = `<h3 class="msg">Oops! Could not find the movie. Try searching another movie.</h3>`;
+          }
+        })
+        .catch(error => {
+          resultDiv.innerHTML = `<h3 class="msg">Oops! An error occurred. Please try again later.</h3>`;
+          console.error(error);
+        });
     }
-    else{
-    let url = `https://www.omdbapi.com/?t=${movienames}&apikey=2921c1e`
-    fetch(url).then(response => response.json()).then((data =>{
-   
-        
-        resultDiv.innerHTML =  `  
-        <div class = "info">     
-          <img src=${data.Poster} class = "poster">
-        <div>    
-        <h2>${data.Title}</h2>
-        <div class = "rating">     
-        <img src="star1.png">
-        <h4>${data.imdbRating}</h4>
-      </div>  
-      <div class = "details">
-      <span>${data.Rated}</span>
-      <span>${data.Year}</span>
-      <span>${data.Runtime}</span>
-        </div>
-        <div class="genre">
-        <div>${data.Genre.split(",").join("</div><div>")}</div>
-        </div>
-        <div class="actors">
-        <div>${data.Actors.split(",").join("</div><div>")}</div>
-    </div>    
-        `
-
-    }))
-
-}
-
-}
+  }
 
 button.addEventListener('click',fetchmovie)
-window.addEventListener("load", getMovie);
+// window.addEventListener("load", getMovie);
 
